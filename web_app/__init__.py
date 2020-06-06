fdbb2349e0f24ee3c145005c1d3f5107a45b6eeb
techthumb1 <robinsonjason761@gmail.com>
2020-06-05 20:00:40 -0500

    Upload files

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,14 +1,22 @@
 # web_app/__init__.py
-
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
-# from web_app01.routes.book_routes import book_routes
+from web_app.routes.book_routes import book_routes
+
+DATABASE_URI = "sqlite:///C:r\\Users\\Username\\Desktop\\web_app01\\web_app_99.db"
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
-    #app.register_blueprint(book_routes)
+    app.register_blueprint(book_routes)
+
     return app
 
 if __name__ == "__main__":
--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -2,5 +2,7 @@
 
 import basilica
 with basilica.Connection('579a7484-3ec5-33f4-cd05-f72712c2590e') as c:
-    embeddings = c.embed_sentences(["Hello world!", "How are you?"])
+    print(type(connection))
+    sentences = ['Hello World!', 'How are you?']
+    embeddings = connection.embed_sentences(sentences)
     print(list(embeddings))
